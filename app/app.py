b198801b2c2d6e73d8560b133bc03f4dddc0fda3
aditigulekar7 <aditigulekar7@gmail.com>
2026-06-03 12:52:05 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -97,7 +97,7 @@ if page == "🏠 Home":
         unsafe_allow_html=True
     )
 
-    st.write("### Machine Learning Based Solar Power Forecasting")
+    st.write("### achine Learning Based Prediction of Solar Energy Generation")
 
     st.info("""
     Predict solar energy generation using weather
